376c81050911e1e7edfa833f9a68f4c6a226406e
Kaifan Zheng <kaifan.zheng@mail.mcgill.ca>
2023-10-08 19:56:40 -0400

    complete satellite controller. one test not pass
    test_update_satellite_info_to_database not pass due to one_to_one relationship not estabilished

--- Satellite_Operations_Services_Optimizer/state_model/tests.py
+++ Satellite_Operations_Services_Optimizer/state_model/tests.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from django.test import TestCase
-from .views import add_satellite, get_satellite_by_id
-from .models import Satellite
+from .views import add_satellite, get_satellite_by_id,get_all_satellites, delete_satellite_by_id, update_satellite_info
+from .models import Satellite, SatelliteSchedule
 # Create your tests here.
 class SatelliteControllerTest(TestCase):
     def test_add_one_satellite_into_database(self):
@@ -12,3 +13,36 @@ class SatelliteControllerTest(TestCase):
         self.assertEqual(test_satellite.storageCapacity,satellite.storageCapacity)
         self.assertEqual(test_satellite.powerCapacity,satellite.powerCapacity)
         self.assertEqual(test_satellite.fieldOfView,satellite.fieldOfView)
+    def test_get_all_satellite_from_database(self):
+        number_of_satellite = 5
+        add_satellite("soso1","this is a TLE.",10,10,10)
+        add_satellite("soso2","this is a TLE.",10,10,10)
+        add_satellite("soso3","this is a TLE.",10,10,10)
+        add_satellite("soso4","this is a TLE.",10,10,10)
+        add_satellite("soso5","this is a TLE.",10,10,10)
+        self.assertEqual(len(get_all_satellites()),number_of_satellite)
+    def test_delete_satellite_by_id_from_database(self):
+        number_of_satellite = 4
+        add_satellite("soso1","this is a TLE.",10,10,10)
+        add_satellite("soso2","this is a TLE.",10,10,10)
+        add_satellite("soso3","this is a TLE.",10,10,10)
+        add_satellite("soso4","this is a TLE.",10,10,10)
+        add_satellite("soso5","this is a TLE.",10,10,10)
+        delete_satellite_by_id("soso3")
+        self.assertEqual(len(get_all_satellites()),number_of_satellite)
+    def test_update_satellite_info_to_database(self):
+        add_satellite("soso1","this is a TLE.",10,10,10)
+        satelliteSchedule = SatelliteSchedule(scheduleID = "station1",satellite = get_satellite_by_id("soso1"),activityWindow=datetime(2023, 10, 15, 14, 30))
+        satelliteSchedule.save()
+        update_satellite_info(
+                            satellite_id = "soso1",
+                            TLE = "this is a TLE2.", 
+                            storageCapacity = 10, 
+                            powerCapacity = 10, 
+                            fieldOfView = 10,
+                            satelliteSchedule = satelliteSchedule
+                            )
+        satellite = get_satellite_by_id("soso1")
+        self.assertEqual(satellite.TLE,"this is a TLE2.")
+        self.assertEqual(satellite.satelliteSchedule.scheduleID,"station1")
+        
--- Satellite_Operations_Services_Optimizer/state_model/views.py
+++ Satellite_Operations_Services_Optimizer/state_model/views.py
@@ -6,7 +6,7 @@ from .models import Satellite, SatelliteSchedule
 from django.utils import timezone
 
 
-
+#satellite controller--------------------
 def add_satellite(satelliteId, TLE, storageCapacity, powerCapacity, fieldOfView):
     new_satellite = Satellite(satelliteId=satelliteId,
                               TLE=TLE,
@@ -15,8 +15,30 @@ def add_satellite(satelliteId, TLE, storageCapacity, powerCapacity, fieldOfView)
                               fieldOfView=fieldOfView)
     new_satellite.save()
 
+def get_all_satellites():
+    return Satellite.objects.all()
 
 def get_satellite_by_id(satellite_id) -> Satellite:
     satellite = Satellite.objects.get(satelliteId=satellite_id)
     return satellite
+def update_satellite_info(satellite_id,TLE, storageCapacity, powerCapacity, fieldOfView,satelliteSchedule):
+    satellite = Satellite.objects.get(satelliteId=satellite_id)
+    satellite.satelliteId = satellite.satelliteId
+    satellite.TLE = TLE
+    satellite.storageCapacity = storageCapacity
+    satellite.powerCapacity = powerCapacity
+    satellite.fieldOfView = fieldOfView
+    satellite.satelliteSchedule = satelliteSchedule
+    satellite.save()
+
+def delete_satellite_by_id(satellite_id):
+    try:
+        satellite = Satellite.objects.get(satelliteId=satellite_id)
+        satellite.delete()
+    except Satellite.DoesNotExist:
+        pass
+#satelliteSchedule controller---------------
+def add_SatelliteSchedule(scheduleId, activityWindow):
+    satelliteSchedule = SatelliteSchedule(scheduleId = scheduleId, activityWindow = activityWindow)
+    satelliteSchedule.save()
 
