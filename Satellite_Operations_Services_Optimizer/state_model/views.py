6a13bd85075b842f55baf1a27f0a5be47da2ca91
Kaifan Zheng <kaifan.zheng@mail.mcgill.ca>
2023-10-09 03:37:18 -0400

    add satelliteSchedule controller
    add satelliteSchedule controller

--- Satellite_Operations_Services_Optimizer/state_model/views.py
+++ Satellite_Operations_Services_Optimizer/state_model/views.py
@@ -14,6 +14,7 @@ def add_satellite(satelliteId, TLE, storageCapacity, powerCapacity, fieldOfView)
                               powerCapacity=powerCapacity,
                               fieldOfView=fieldOfView)
     new_satellite.save()
+    return new_satellite
 
 def get_all_satellites():
     return Satellite.objects.all()
@@ -38,7 +39,27 @@ def delete_satellite_by_id(satellite_id):
     except Satellite.DoesNotExist:
         pass
 #satelliteSchedule controller---------------
-def add_SatelliteSchedule(scheduleId, activityWindow):
-    satelliteSchedule = SatelliteSchedule(scheduleId = scheduleId, activityWindow = activityWindow)
+def add_SatelliteSchedule(scheduleId, activityWindow, satellite):
+    satelliteSchedule = SatelliteSchedule(scheduleId = scheduleId, activityWindow = activityWindow, satellite = satellite)
     satelliteSchedule.save()
+    return satelliteSchedule
+ 
+def get_all_satelliteSchedules():
+    return SatelliteSchedule.objects.all()
 
+def get_satelliteSchedule_by_id(scheduleId)->SatelliteSchedule:
+    return Satellite.objects.get(scheduleId)
+
+def update_satelliteSchedule_info(scheduleId, activityWindow, satellite):
+    satelliteSchedule = SatelliteSchedule.objects.get(scheduleId)
+    satelliteSchedule.scheduleID = satelliteSchedule.scheduleID
+    satelliteSchedule.activityWindow = activityWindow
+    satelliteSchedule.satellite = satellite
+    satellite.save()
+
+def delete_satelliteSchedule_by_id(scheduleId):
+    try:
+        satelliteSchedule = SatelliteSchedule.objects.get(scheduleId)
+        satelliteSchedule.delete()
+    except satelliteSchedule.DoesNotExist:
+        pass
