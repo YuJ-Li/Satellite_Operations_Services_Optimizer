ff4d7aedbd56b47b073f91d4cec80427a729bd98
Noshin03 <69012995+Noshin03@users.noreply.github.com>
2023-10-30 10:03:29 -0400

    https requests

--- Satellite_Operations_Services_Optimizer/state_model/serializers.py
+++ Satellite_Operations_Services_Optimizer/state_model/serializers.py
@@ -1,12 +1,47 @@
 from rest_framework import serializers
-from .models import Satellite, SatelliteSchedule
+from .models import Satellite, SatelliteSchedule, ImagingTask,MaintenanceTask, DownlinkTask,GroundStation, GroundStationRequest, Image, Outage, SatelliteTask
 
 class SatelliteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Satellite
-        fields = '__all__'
+        fields = '_all_'
 
 class SatelliteScheduleSerializer(serializers.ModelSerializer):
     class Meta:
         model = SatelliteSchedule
-        fields = '__all__'
+        fields = '_all_'
+
+class ImagingTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImagingTask
+        fields = '_all_'
+
+class MaintenanceTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MaintenanceTask
+        fields = '_all_'
+
+class DownlinkTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DownlinkTask
+        fields = '_all_'
+
+class GroundStationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroundStation
+        fields = '_all_'
+
+class GroundStationRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GroundStationRequest
+        fields = '_all_'
+    
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '_all_'
+
+class OutageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Outage
+        fields = '_all_'
--- Satellite_Operations_Services_Optimizer/state_model/views.py
+++ Satellite_Operations_Services_Optimizer/state_model/views.py
@@ -7,17 +7,18 @@ from django.utils import timezone
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
-from .serializers import SatelliteSerializer, SatelliteScheduleSerializer
+from .serializers import *
 from rest_framework import status
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+#test restAPI-----------------------------------------
 @api_view(['GET'])
 def getData(request):
     person = {'name':'kai','age':28}
     return Response(person)
-
+#satellite restAPI-----------------------------------------
 @api_view(['GET', 'POST'])
 def satellite_list(request):
     if request.method == 'GET':
@@ -56,7 +57,8 @@ def satellite_detail(request, satellite_id):
 
     except Satellite.DoesNotExist:
         raise Http404("Satellite not found")
-
+    
+#satelliteScedule restAPI-----------------------------------------
 @api_view(['GET', 'POST'])
 def satellite_schedule_list(request):
     if request.method == 'GET':
@@ -66,9 +68,9 @@ def satellite_schedule_list(request):
 
     elif request.method == 'POST':
 
-        # satellite_id = request.data.get('satellite')
-        # if not Satellite.objects.filter(satelliteId=satellite_id).exists():
-        #     return JsonResponse({'error': 'Satellite not found'}, status=400)
+        satellite_id = request.data.get('satellite')
+        if not Satellite.objects.filter(id=satellite_id).exists():
+            return JsonResponse({'error': 'Satellite not found'}, status=400)
 
         serializer = SatelliteScheduleSerializer(data=request.data)
         if serializer.is_valid():
@@ -102,3 +104,143 @@ def satellite_schedule_detail(request, schedule_id):
 
     except SatelliteSchedule.DoesNotExist:
         raise Http404("Schedule not found")
+#imaging task restAPI-----------------------------------------
+@api_view(['GET', 'POST'])
+def imaging_task_list(request):
+    if request.method == 'GET':
+        tasks = ImagingTask.objects.all()
+        serializer = ImagingTaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        schedule_id = request.data.get('schedule')
+        if not SatelliteSchedule.objects.filter(id=schedule_id).exists():
+            return JsonResponse({'error': 'SatelliteSchedule not found'}, status=400)
+
+        serializer = ImagingTaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def imaging_task_detail(request, task_id):
+    try:
+        task = ImagingTask.objects.get(TaskID=task_id)
+
+        # For GET requests
+        if request.method == 'GET':
+            serializer = ImagingTaskSerializer(task)
+            return JsonResponse(serializer.data)
+
+        # For PUT requests
+        elif request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = ImagingTaskSerializer(task, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
+
+        # For DELETE requests
+        elif request.method == 'DELETE':
+            task.delete()
+            return HttpResponse(status=204)
+
+    except ImagingTask.DoesNotExist:
+        raise Http404("imagetask not found")
+
+#downlink task restAPI-----------------------------------------
+@api_view(['GET', 'POST'])
+def downlink_task_list(request):
+    if request.method == 'GET':
+        tasks = DownlinkTask.objects.all()
+        serializer = DownlinkTaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        schedule_id = request.data.get('schedule')
+        if not SatelliteSchedule.objects.filter(id=schedule_id).exists():
+            return JsonResponse({'error': 'SatelliteSchedule not found'}, status=400)
+
+        serializer = DownlinkTaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def downlink_task_detail(request, task_id):
+    try:
+        task = DownlinkTask.objects.get(TaskID=task_id)
+
+        # For GET requests
+        if request.method == 'GET':
+            serializer = DownlinkTaskSerializer(task)
+            return JsonResponse(serializer.data)
+
+        # For PUT requests
+        elif request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = DownlinkTaskSerializer(task, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
+
+        # For DELETE requests
+        elif request.method == 'DELETE':
+            task.delete()
+            return HttpResponse(status=204)
+
+    except DownlinkTask.DoesNotExist:
+        raise Http404("downlink task not found")
+    
+#MaintenanceTask restAPI-----------------------------------------
+@api_view(['GET', 'POST'])
+def maintenance_task_list(request):
+    if request.method == 'GET':
+        tasks = MaintenanceTask.objects.all()
+        serializer = MaintenanceTaskSerializer(tasks, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        schedule_id = request.data.get('schedule')
+        if not SatelliteSchedule.objects.filter(id=schedule_id).exists():
+            return JsonResponse({'error': 'SatelliteSchedule not found'}, status=400)
+
+        serializer = MaintenanceTaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def maintenance_task_detail(request, task_id):
+    try:
+        task = MaintenanceTask.objects.get(TaskID=task_id)
+
+        # For GET requests
+        if request.method == 'GET':
+            serializer = MaintenanceTaskSerializer(task)
+            return JsonResponse(serializer.data)
+
+        # For PUT requests
+        elif request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = MaintenanceTaskSerializer(task, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+            return JsonResponse(serializer.errors, status=400)
+
+        # For DELETE requests
+        elif request.method == 'DELETE':
+            task.delete()
+            return HttpResponse(status=204)
+
+    except MaintenanceTask.DoesNotExist:
+        raise Http404("downlink task not found")
