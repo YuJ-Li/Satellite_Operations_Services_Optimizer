d4aa257a6bba4c513a68385876333fd4b8c08c25
chun-li9 <chun160712@gmail.com>
2023-11-13 18:32:10 -0500

    EDF worked

--- Satellite_Operations_Services_Optimizer/state_model/edf_priority.py
+++ Satellite_Operations_Services_Optimizer/state_model/edf_priority.py
@@ -4,7 +4,7 @@ class Task:
         self.start_time = start_time
         self.end_time = end_time
         self.duration = duration
-        self.priority = priority
+        self.priority = priority # priority from 3 to 1 = from high to low
         self.satellite = satellite
 
 class Satellite:
@@ -13,37 +13,16 @@ class Satellite:
         self.activity_window = activity_window
         self.schedule = [] # list of ((#task_name, actual_start_time, real_end_time))
 
-# priority from 5 to 1 = from high to low
-tasks = [
-        Task("Task1", start_time=0, end_time=3, duration=2, priority=5, satellite='1'),
-        Task("Task2", start_time=2, end_time=7, duration=2, priority=2),
-        Task("Task3", start_time=5, end_time=7, duration=1, priority=1),
-        Task("Task4", start_time=8, end_time=12, duration=2, priority=4),
-        Task("Task5", start_time=1, end_time=5, duration=2, priority=4),
-        Task("Task6", start_time=6, end_time=10, duration=3, priority=3),
-        Task("Task7", start_time=2, end_time=6, duration=2, priority=2),
-        Task("Task8", start_time=4, end_time=9, duration=2, priority=1),
-        Task("Task9", start_time=9, end_time=12, duration=2, priority=4),
-        Task("Task10", start_time=1, end_time=4, duration=2, priority=3),
-        Task("Task11", start_time=5, end_time=9, duration=1, priority=2),
-        Task("Task12", start_time=3, end_time=8, duration=4, priority=1),
-        Task("Task13", start_time=7, end_time=11, duration=2, priority=4),
-        Task("Task14", start_time=10, end_time=15, duration=3, priority=4),
-        Task("Task15", start_time=2, end_time=5, duration=2, priority=3),
-        Task("Task16", start_time=6, end_time=9, duration=1, priority=2),
-        Task("Task17", start_time=4, end_time=7, duration=1, priority=1),
-        Task("Task18", start_time=8, end_time=11, duration=2, priority=4),
-        Task("Task19", start_time=1, end_time=6, duration=2, priority=4),
-        Task("Task20", start_time=3, end_time=8, duration=2, priority=3),
-    ]
 
-priority_list = {}
-for task in tasks:
-    if priority_list.get(task.priority) is None:
-        priority_list[task.priority] = [task]
-    else:
-        priority_list[task.priority].append(task)
-priority_list = dict(sorted(priority_list.items(), key=lambda item: item[0], reverse=True))
+def group_by_priority(task_list):
+    priority_list = {}
+    for task in task_list:
+        if priority_list.get(task.priority) is None:
+            priority_list[task.priority] = [task]
+        else:
+            priority_list[task.priority].append(task)
+    priority_list = dict(sorted(priority_list.items(), key=lambda item: item[0], reverse=True))
+    return priority_list
 
 def print_priority_list(priority_list):
     for p_group in priority_list.items():
@@ -52,7 +31,6 @@ def print_priority_list(priority_list):
             s = s + t.name + ", "
         print(s)
 
-print_priority_list(priority_list)
 
 def edf(priority_list, satellites):
     ''' priority_tasks is a dictionary of tasks grouped by priority'''
@@ -60,20 +38,37 @@ def edf(priority_list, satellites):
     for p_group in priority_list.items(): 
         tasks = p_group[1]
         tasks.sort(key=lambda x: (x.end_time,x.start_time)) # for each priority group, sort tasks by end time then by start time
+        s = str(p_group[0]) + ": "
+        for t in tasks:
+            s = s + t.name + ", "
+        print(s)
         for task in tasks:
             scheduled = False
             for satellite in satellites:
+                if task.satellite is not None and task.satellite != satellite: continue
                 schedule_ptr = -1
-                while not scheduled or schedule_ptr==len(satellite.schedule):
+                while not scheduled and schedule_ptr<len(satellite.schedule):
                     empty_slot_start, empty_slot_end, schedule_ptr = find_next_slot(satellite, schedule_ptr)
-             #TODO       if empty_slot_end-empty_slot_start >= task.duration and task.start_time >= empty_slot_start and task.end_time <= empty_slot_end:
-                        scheduled_start = task.start_time if task.start_time>empty_slot_start else #TODO
-                        satellite.schedule.insert(schedule_ptr, (task.name, )) #TODO
-                        scheduled = True
-                        # TODO: remove this task from the task list
-                        print(f'{task.name} is scheduled.')
-                        break
+                    if empty_slot_start is not None and empty_slot_end is not None:
+                        if task.start_time < empty_slot_start and empty_slot_start + task.duration <= empty_slot_end and empty_slot_start + task.duration <= task.end_time:
+                            scheduled_start = empty_slot_start
+                            scheduled_end = scheduled_start + task.duration
+                            satellite.schedule.insert(schedule_ptr, (task.name, scheduled_start, scheduled_end)) 
+                            scheduled = True
+                            # TODO: remove this task from the task list
+                            print(f'{task.name} is scheduled on {satellite.name}.')
+                            break
+                        elif task.start_time >= empty_slot_start and task.start_time + task.duration <= empty_slot_end and task.start_time + task.duration <= task.end_time:
+                            scheduled_start = task.start_time
+                            scheduled_end = scheduled_start + task.duration
+                            satellite.schedule.insert(schedule_ptr, (task.name, scheduled_start, scheduled_end)) 
+                            scheduled = True
+                            # TODO: remove this task from the task list
+                            print(f'{task.name} is scheduled on {satellite.name}.')
+                            break
                 if scheduled: break
+            if not scheduled:
+                print(f'Failed to schedule {task.name}.')
 
     
 def find_next_slot(satellite, ptr):
@@ -84,16 +79,46 @@ def find_next_slot(satellite, ptr):
     if ptr == -1: # if pointer is pointing at the beginning of the schedule
         if satellite_schedule[0][1] - satellite.activity_window[0] > 0: # if there is space between the start of activity window and the start of first task
             return satellite.activity_window[0], satellite_schedule[0][1], ptr+1
+        ptr += 1
     for i in range(ptr, len(satellite_schedule)-1):
         if satellite_schedule[i+1][1] - satellite_schedule[i][2] > 0: # if there is time between the start of next task and the end of this task
             return satellite_schedule[i][2],satellite_schedule[i+1][1], i+1
+        ptr += 1
     if ptr == len(satellite_schedule)-1:
         if satellite.activity_window[1] - satellite_schedule[ptr][2] > 0:
             return satellite_schedule[ptr][2], satellite.activity_window[1], ptr+1
+    return None, None, ptr+1
         
 
+satellites = [Satellite('S1',(0,23)),Satellite('S2',(0,23)),Satellite('S3',(0,23)),Satellite('S4',(0,23)),Satellite('S5',(0,23))]
+tasks = [
+        Task("Task1", start_time=0, end_time=3, duration=3, priority=5, satellite=satellites[0]),
+        Task("Task2", start_time=2, end_time=7, duration=5, priority=2),
+        Task("Task3", start_time=5, end_time=7, duration=2, priority=1),
+        Task("Task4", start_time=8, end_time=12, duration=4, priority=4),
+        Task("Task5", start_time=1, end_time=5, duration=4, priority=4),
+        Task("Task6", start_time=6, end_time=10, duration=4, priority=3),
+        Task("Task7", start_time=2, end_time=6, duration=4, priority=2),
+        Task("Task8", start_time=4, end_time=9, duration=5, priority=1),
+        Task("Task9", start_time=9, end_time=12, duration=3, priority=4),
+        Task("Task10", start_time=1, end_time=4, duration=3, priority=3),
+        Task("Task11", start_time=5, end_time=9, duration=4, priority=2),
+        Task("Task12", start_time=3, end_time=8, duration=5, priority=1),
+        Task("Task13", start_time=7, end_time=11, duration=4, priority=4),
+        Task("Task14", start_time=10, end_time=15, duration=5, priority=4),
+        Task("Task15", start_time=2, end_time=5, duration=3, priority=3),
+        Task("Task16", start_time=6, end_time=9, duration=3, priority=2),
+        Task("Task17", start_time=4, end_time=7, duration=3, priority=1),
+        Task("Task18", start_time=8, end_time=11, duration=3, priority=4),
+        Task("Task19", start_time=1, end_time=5, duration=4, priority=4),
+        Task("Task20", start_time=3, end_time=8, duration=5, priority=3),
+]
 
+priority_list = group_by_priority(tasks)
 
-satellites = [Satellite(1,(0,23)),Satellite(2,(0,23)),Satellite(3,(0,23)),Satellite(4,(0,23)),Satellite(5,(0,23))]
+# print_priority_list(priority_list)
 
 edf(priority_list, satellites)
+
+for satellite in satellites:
+    print(satellite.schedule)
--- Satellite_Operations_Services_Optimizer/state_model/try.py
+++
@@ -1,6 +0,0 @@
-s = [1,2,3,4,5]
-s.insert(0,0)
-print(s)
-s.insert(7,9)
-print(s)
-
